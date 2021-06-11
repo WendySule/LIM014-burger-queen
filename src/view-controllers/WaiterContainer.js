997f12561183ca06b5bf926dde00eb2c2b64fd9c
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-06-10 20:34:57 -0500

    change className

--- src/view-controllers/WaiterContainer.js
+++ src/view-controllers/WaiterContainer.js
@@ -5,12 +5,20 @@ function WaiterContainer() {
     <div>
         <section className="waiter-container">
             <article>
-                <ul>
+                <ul className="select-menu">
                     <li><img src="https://image.flaticon.com/icons/png/512/1037/1037762.png" alt="icon-select-menu"/>menú</li>
                     <li><img src="https://image.flaticon.com/icons/png/512/887/887359.png" alt="icon-select-breakfast"/>desayuno</li>
                 </ul>
             </article>
-            <article>
+            <article className="select-breakfast">
+              <ul>
+                <li><img src="https://image.flaticon.com/icons/png/512/43/43869.png" alt="more-icon"/>
+                  Datos del Pedido
+                </li>
+                <li><img src="https://image.flaticon.com/icons/png/512/43/43869.png" alt="more-icon"/>
+                  Productos del Cliente
+                </li>
+              </ul>
             </article>
         </section>
     </div>
