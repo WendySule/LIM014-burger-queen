acc98865f28d8ecf9a4ec65d2844707b211b238d
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-06-14 18:06:17 -0500

    create collections

--- src/collections/functions.js
+++ src/collections/functions.js
@@ -1,6 +1,26 @@
-import { createUser } from './firestore-controller'
+import { createProduct } from './firestore-controller'
 
-export const desayuno = (src, name, price, info) => {
-    createUser(src, name, price, info)
-    .then(() => {console.log('funciona')})
+export const desayuno = ( src, name, price, info, id) => {
+    const img = src
+    const nameId = name
+    const priceId = price
+    const infoId = info 
+    const ProductId = id 
+    const descriptionId = 'desayuno' 
+    createProduct(img, nameId, priceId, infoId, ProductId, descriptionId)
 }
+export const menu = ( src, name, price, info, id) => {
+    const img = src
+    const nameId = name
+    const priceId = price
+    const infoId = info 
+    const ProductId = id 
+    const descriptionId = 'menu' 
+    createProduct(img, nameId, priceId, infoId, ProductId, descriptionId)
+}
+// desayuno("https://images.unsplash.com/photo-1588012841523-08163e2b0c26?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+// "Café americano",
+// '5',
+// 'bebida',
+// '001'
+// )
