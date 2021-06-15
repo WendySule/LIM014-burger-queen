765f4b123de025c032abc7131f9cbbea58097320
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-06-14 21:57:21 -0500

    get collection items menu database & render

--- src/view-controllers/WaiterContainer.js
+++ src/view-controllers/WaiterContainer.js
@@ -1,54 +1,46 @@
-import React, { useState, useEffect  } from 'react';
-import Burger from './Order';
-import Cart from './Cart';
+import React, { useEffect, useState } from 'react';
+import { db } from '../collections/firebase-config'
+import Burger from './Burger'
+import Cart from './Cart'
 
 function WaiterContainer() {
-  const [burgers, setBurgers] = useState([
-    { id: 1,  nombre: "SALTEÑA", precio: 250 },
-    { id: 2,  nombre: "PORTEÑA", precio: 180 },
-    { id: 3,  nombre: "CHICKEN", precio: 150 },
-    { id: 4,  nombre: "CRIOLLA", precio: 200 },
-    { id: 5,  nombre: "PATAGONIA", precio: 240 },
-  ]);
-  const [cart, setCart] = useState([]); 
+const [menu, setMenu] = useState([])
+const [cart, setCart] = useState([])
+const [type, setType] = useState('menu')
 
   useEffect(() => {
-    const getData = async() => {
-      const url = 'https://burger-queen-45750-default-rtdb.firebaseio.com/productos.json'
-      const response = await fetch(url)
-      const data = await response.json()
-      const arrayBreakfast = Object.values(data.desayuno);
-      const arrayMenu = Object.values(data.menu);
-      console.log(data)
-      const newArray = arrayBreakfast.concat(arrayMenu);
-      setBurgers(newArray)
-     // console.log(newArray)
-    };
-      getData();
-    //setBurgers([getData()])
-  }, []
-  );
+    var docRef = db.collection('menu')
 
-
+    docRef.get().then((doc) => {
+      const documents = []
+      doc.forEach(e => {
+        documents.push({ id: e.id, ...e.data()})
+      })
+      setMenu(documents.filter(e => e.category === type))
+      //setMenu(documents)
+      //console.log(setMenu)
+    })
+  }, [type])
+  //console.log(useEffect)
   return (
       <div>
           <section className="waiter-container">
               <article>
                   <ul className="select-menu">
-                      <li><img src="https://image.flaticon.com/icons/png/512/1037/1037762.png" alt="icon-select-menu"/>menú</li>
-                      <li><img src="https://image.flaticon.com/icons/png/512/887/887359.png" alt="icon-select-breakfast"/>desayuno</li>
+                      <li onClick={()=>{setType('menu')}}><img src="https://image.flaticon.com/icons/png/512/1037/1037762.png" alt="icon-select-menu"/>menú</li>
+                      <li onClick={()=>{setType('desayuno')}}><img src="https://image.flaticon.com/icons/png/512/887/887359.png" alt="icon-select-breakfast"/>desayuno</li>
                   </ul>
                   <div className="gallery">
                       <div className="gallery-container">
-                      { burgers.map((burger, index) => (
+                      {menu.map((e) => (
                         <Burger
-                          key={index}
-                          burger={burger}
-                          cart={cart}
+                          key= {e.id}
+                          burger = {e}
+                          cart = {cart}   
                           setCart={setCart}
-                          burgers={burgers}
-                        />
-                      ))}
+                          burgers={menu}   
+                          />
+                        ))}
                       </div>
                   </div>
               </article>
